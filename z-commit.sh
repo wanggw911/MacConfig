43e737af8d0ea80f65642d45767d8ebf33bc9702
wanggw911 <wangguowen911@gmail.com>
2018-12-04 16:25:25 +0800

    添加git提交脚本

---
+++ z-commit.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+
+currentDir=$(cd "$(dirname "$0")"; pwd)
+cd ${currentDir}
+
+
+echo "\033[32m请输入提交信息：🙃 ====================================================\033[0m"
+read commitInfo
+
+
+echo "\033[32mgit 开始更新....🙃 ==================================================\033[0m"
+git add .
+git commit -m $commitInfo
+git push -u origin master
+
+
+echo "\033[32mgit 推送完毕🙃 ======================================================\033[0m"
+read -p "提交完成  🎉  🎉  🎉  🎈  🎈  🎈 Press any key to exit." var
+exit
